dd23939f37498e55ba33dd866bbf8eec1dea652b
Ramy-Basuoney <69299605+Ramy-Basuoney@users.noreply.github.com>
2021-12-08 17:24:20 +0200

    fix
    fixing see more btn issues

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const bars = document.querySelector(".header__bars");
 const list = document.querySelector(".nav__list");
 const links = document.querySelectorAll(".nav__link");
 const seeMore = document.querySelector(".project__btn");
-const hideProject = document.querySelector(".hide-project");
+const hideProject = document.querySelectorAll(".hide-project");
 
 // FUNCTIONS
 bars.addEventListener("click", function () {
@@ -18,6 +18,7 @@ bars.addEventListener("click", function () {
 // SEE MORE
 seeMore.addEventListener("click", function (e) {
   e.preventDefault();
-
-  hideProject.classList.toggle("hidden");
+  hideProject.forEach((pro) => {
+    pro.classList.toggle("hidden");
+  });
 });
